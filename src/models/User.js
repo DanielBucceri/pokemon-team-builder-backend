d8838a16d39e986aea1336b0019d32acf33b24b7
Daniel <gcas012125@coderacademy.edu.au>
2025-05-25 00:29:40 +1000

    setting up User model

---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true,
+    trim: true,
+    minlength: [3, 'Username must be at least 3 characters long']
+  },
+  passwordHash: {
+    type: String,
+    required: [true, 'Password is required']
+  }
+}, {
+  timestamps: true
+});
